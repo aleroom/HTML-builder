331cd179febd262a027b673f384e3a63f9571b39
aleroom <aleroom@yahoo.com>
2025-01-17 14:59:40 +0300

    feat: add 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,3 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
+let filePath = path.join(__dirname, 'text.txt');
+
+let readStream = fs.createReadStream(filePath, 'utf-8');
+readStream.on('data', (chunk) => {
+    console.log(chunk);
+});
+
+readStream.on('error', (err) => {
+    console.error('Ошибка при чтении файла:', err);
+});
